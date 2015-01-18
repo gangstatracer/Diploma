9ff16e65037224b60648e3225a0f6facdfe0c6f9
gangstatracer <gangstatracer@gmail.com>
2015-01-18 17:09:15 +0500

    Engine proto.

--- genetic_engine.py
+++ genetic_engine.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from pyevolve import GenomeBase
+from pyevolve import GenomeBase, Util
 from flow import Flow
 from fx import FFlow
 from nets_manager import cls_ranges
@@ -9,7 +9,7 @@ from nets_manager import cls_ranges
 
 class NetworkGenome(GenomeBase):
     """
-    Класс, представляющий соболь модель реальной сети, для эволюционирования в pyevolve
+    Класс, представляющий собой модель реальной сети, для эволюционирования в pyevolve
     """
     def __init__(self, nets, nodes, flows, fflow, texp):
         super(NetworkGenome, self).__init__()
@@ -46,16 +46,16 @@ class NetworkGenome(GenomeBase):
         self.genome = self.fxs + [self.fflow]
 
         # TODO: реализовать операции над особями
-        self.initializator.set(AttackInitializator)
-        self.mutator.set(AttackMutator)
-        self.crossover.set(AttackCrossover)
-        self.evaluator.set(eval_func)
+        self.initializator.set(network_initializer)
+        self.mutator.set(network_mutator)
+        self.crossover.set(network_crossover)
+        self.evaluator.set(network_tester)
 
     # Реализация контракта pyevolve
     def copy(self, g):
         g.nets = self.nets[:]
         g.nodes = self.nodes[:]
-        g.flows = self.flows[:]
+        g.flows = map(lambda fl: fl.clone(), self.flows)
         g.fflow = self.fflow.clone()
         g.texp = self.texp
 
@@ -71,3 +71,56 @@ class NetworkGenome(GenomeBase):
         clone = NetworkGenome(self.nets, self.nodes, self.flows, self.fflow, self.texp)
         self.copy(clone)
         return clone
+
+
+def network_mutator(genome, **args):
+    if args["pmut"] <= 0.0:
+        return 0
+    list_size = len(genome)
+    mutations = args["pmut"] * list_size
+
+    if mutations < 1.0:
+        mutations = 0
+        for it in xrange(list_size):
+            if Util.randomFlipCoin(args["pmut"]):
+                genome[it] = 1  # TODO rand_init(genome, it)
+            mutations += 1
+
+    else:
+        for it in xrange(int(round(mutations))):
+            which_gene = 1  # TODO rand_randint(0, list_size - 1)
+            genome[which_gene] = 1  # TODO rand_init(genome, which_gene)
+
+    return mutations
+
+
+def network_crossover(genome, **args):
+    # TODO
+    g_mom = args["mom"]
+    g_dad = args["dad"]
+
+    sister = g_mom.clone()
+    brother = g_dad.clone()
+    sister.resetStats()
+    brother.resetStats()
+
+    for i in xrange(len(g_mom)):
+        if Util.randomFlipCoin(0.5):
+            temp = sister[i]
+            sister[i] = brother[i]
+            brother[i] = temp
+
+    return sister, brother
+
+
+def network_initializer(genome, **args):
+    for i in xrange(len(genome)):
+        genome = 1  # TODO rand_init(genome, i)
+
+
+def network_tester(genome):
+    # TODO
+    score = 0.0
+
+    return score
+
